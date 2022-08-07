251e830fcadb6989f2caa14ea5a5049b3fb0deb6
khanhionia <khanhioniakira@gmail.com>
2022-08-07 20:49:53 +0700

    Create User and Read User with database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,12 @@
 import express from "express";
 import db from '../models/index';
+import CRUDService from '../services/CRUDService';
 
 let getHomePage = async (req, res) => {
     try {
-        let data = await db.User.findAll();
+        let data = await db.User.findAll({
+            raw: true
+        });
         console.log('>>>check data:', data);
         return res.render('homePage.ejs', {
             data: JSON.stringify(data)
@@ -16,7 +19,28 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
+}
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    });
+}
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,9 +6,9 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
-    router.get('/hoiDanIT', (req, res) => {
-        return res.send('Hello world with HoiDanIT');
-    });
+    router.get('/crud', homeController.getCRUD);
+    router.post('/post-crud', homeController.postCRUD);
+    router.get('/get-crud', homeController.displayGetCRUD);
 
     //rest api
 
